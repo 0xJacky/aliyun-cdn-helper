5a5f7df5243a2e2a6c995431bd2bb50a9868f2be
Jacky <jacky-943572677@qq.com>
2017-05-22 15:24:26 +0800

    使用 WordPress 内建 admin-ajax，不再引用 jQuery，内建 jQuery 已经可以满足显示 Toastr
    warning: 本版本正在测试中，不建议在生产环境中使用
    另外，原来不安全的操作方法 do.php 将删除，请在更新时注意。

--- refresh_alicdn.php
+++ refresh_alicdn.php
@@ -3,7 +3,7 @@
 Plugin Name: Refresh AliCDN
 Plugin URI: https://jackyu.cn/projects/refresh_alicdn
 Description: A tweak can help you refreshing your aliyun cdn cache without logining to the aliyun console.
-Version: 1.3
+Version: 1.3.1
 Author: Jacky
 Author URI: https://jackyu.cn/
 License: GPL2
@@ -45,7 +45,7 @@ function refresh_alicdn_remove() {
 if ( is_admin() ) {
 	add_action( 'admin_bar_menu', 'refresh_button', 90 );
 	add_action('admin_footer', 'add_toastr_style');
-	add_action('admin_footer', 'show_toastr' );
+	add_action('admin_footer', 'echo_js' );
 	add_action('admin_menu', 'aliyun_cdn_menu');
 }
 
@@ -54,31 +54,24 @@ function refresh_button( $meta = TRUE ) {
   if ( is_super_admin() || is_admin_bar_showing() ) {
     $wp_admin_bar->add_menu( array(
       'id' => 'refresh',
-      'title' => __( '刷新 CDN 缓存' ),
+      'title' => __( '刷新缓存' ),
       'href' => '#',
       'meta'  => array( onclick => 'refresh()' ) )
     );
   }
 }
 
-function add_toastr_style() {
-  if ( is_super_admin() || is_admin_bar_showing() ) {
-    wp_register_style( 'toastrCSS', '//cdn.bootcss.com/toastr.js/latest/css/toastr.min.css' );
-    wp_register_script( 'toastrJS', '//cdn.bootcss.com/toastr.js/latest/js/toastr.min.js' );
-    wp_register_script( 'jqueryJS', '//cdn.bootcss.com/jquery/3.2.1/jquery.min.js' );
-    wp_enqueue_style('toastrCSS');
-    wp_enqueue_script('toastrJS');
-    wp_enqueue_script('jqueryJS');
-  }
-}
-
-function show_toastr() {
-  echo "<script>function refresh() {
-    $.ajax({
-			type:\"post\",
-      url: \"".plugins_url('do.php', __FILE__)."\",
-			data: \"key=".get_option('cdn_post_pw')."\",
-      success: function(data) {
+// 输出 JavaScript 脚本
+function echo_js() {
+  echo "<script type=\"text/javascript\">
+  function refresh() {
+        jQuery.ajax({
+            type: 'POST',
+            url: \"".admin_url( 'admin-ajax.php' )."\",
+            data: {
+                action: 'refresh_cdn'
+            },
+            success: function(data) {
         if( data.result==1 ) {
           toastr.success(data.message);
         }
@@ -89,11 +82,141 @@ function show_toastr() {
 					toastr.warning(data.message);
 				}
       }
-    });}</script>";
+        });
+       }
+    </script>";
+}
+
+function add_toastr_style() {
+  if ( is_super_admin() || is_admin_bar_showing() ) {
+    wp_register_style( 'toastrCSS', '//cdn.bootcss.com/toastr.js/latest/css/toastr.min.css' );
+    wp_register_script( 'toastrJS', '//cdn.bootcss.com/toastr.js/latest/js/toastr.min.js' );
+    wp_enqueue_style('toastrCSS');
+    wp_enqueue_script('toastrJS');
   }
+}
+
+add_action( 'wp_ajax_nopriv_refresh_cdn', 'refresh_cdn' );
+add_action( 'wp_ajax_refresh_cdn', 'refresh_cdn' );
+
+
+function refresh_cdn() {
+	header( "Content-Type: application/json" );
+  	$type = is_numeric(get_option('cdn_refresh_type')) ? esc_attr(get_option('cdn_refresh_type')) : 1;
+  	switch($type){
+    	case 1:
+      		$url = get_stylesheet_directory_uri()."/style.css";
+      	break;
+      	case 2:
+      		$url = explode("\n", get_option('cdn_refresh_urls'));
+       break;
+      	case 3:
+      		$style_url = array(get_stylesheet_directory_uri()."/style.css");
+        	$custom_url = explode("\n", get_option('cdn_refresh_urls'));
+        	$url = array_merge($custom_url, $style_url);
+       break;
+        case 4:
+      		$urls = getDir(get_stylesheet_directory());
+        	$custom_url = explode("\n", get_option('cdn_refresh_urls'));
+        	$url = array_merge($urls, $custom_url);
+      	break;
+  	}
+
+  	//print_r($url);
+	echo do_refresh($url);
+	exit();
+}
+
+function do_refresh($url) {
+  /* 载入 Aliyun CDN SDK */
+      include_once 'aliyun-php-sdk-core/Config.php';
+      include_once 'aliyun-php-sdk-cdn/Request/v20141111/RefreshObjectCachesRequest.php';
+      include_once 'aliyun-php-sdk-cdn/Request/v20141111/DescribeRefreshQuotaRequest.php';
+
+      //getProfile的三个参数分别是：region, Access Key ID, Access Key Secret
+      $iClientProfile = DefaultProfile::getProfile("cn-hangzhou", get_option('cdn_access_key_id'), get_option('cdn_access_key_secret'));
+      $client = new DefaultAcsClient($iClientProfile);
+      $request = new \Cdn\Request\V20141111\RefreshObjectCachesRequest();
+      $Quota = new \Cdn\Request\V20141111\DescribeRefreshQuotaRequest();
+      $Quota->setMethod("GET");
+      $Quota = $client->getAcsResponse($Quota);
+  
+  if ($Quota->UrlRemain >= 0) {
+          if ( is_array($url) ){
+            $urls_num = count($url);
+            for ($i=0;$i<$urls_num;$i++) {
+              $request->setObjectPath($url[$i]);
+              $response = $client->doAction($request);
+              $result = $response->getBody();
+              $oResult = json_decode($result,1);
+              if ($oResult && $oResult["RefreshTaskId"]) {
+                $status = 1;
+                continue;
+              } else {
+                $status = 0;
+                break;
+              }
+            }
+          } else {
+            $request->setObjectPath($url);
+            $response = $client->doAction($request);
+            $result = $response->getBody();
+            $oResult = json_decode($result,1);
+            if ($oResult && $oResult["RefreshTaskId"]) {
+              $status = 1;
+            } else {
+              $status = 0;
+            }
+          }
+          if ( $status == 1 ) {
+            $ret = array();
+            $ret['result'] = 1;
+            $ret['message'] = "缓存刷新成功！";
+            echo json_encode($ret);
+          } elseif ( $status == 0 ) {
+            $ret = array();
+            $ret['result'] = 2;
+            $ret['message'] = "缓存刷新失败！"."<br />错误详情: ".$oResult["Message"];
+            echo json_encode($ret);
+          }
+        } else {
+          $ret = array();
+          $ret['result'] = 3;
+          $ret['message'] = "刷新次数以达上限"."<br />错误详情: ".$oResult["Message"];
+          echo json_encode($ret);
+        }
+}
+
+function getFileType($file) {
+          //获取文件扩展名
+          return substr($file, strrpos($file, '.') + 1);
+        }
+function searchDir($path,&$data){
+          //获取文件名
+          if(is_dir($path)){
+            $dp=dir($path);
+            while($file=$dp->read()){
+              if($file!='.'&& $file!='..'){
+                searchDir($path.'/'.$file, $data);
+              }
+            }
+            $dp->close();
+          }
+          if(is_file($path)){
+            if (getFileType($path) !== "php") { //不刷新 PHP 文件
+              $path = str_replace(ABSPATH, site_url()."/", $path); //将绝对路径替换为 URL 形式
+              $data[]=$path;
+            }
+          }
+        }
+function getDir($dir){
+          $data=array();
+          searchDir($dir,$data);
+          return   $data;
+        }
 
 function aliyun_cdn_menu() {
-		add_action( 'admin_init', 'register_refresh_alicdn_settings' );
+	add_action( 'admin_init', 'register_refresh_alicdn_settings' );
     add_options_page('阿里云 CDN 刷新设置', '阿里云 CDN 刷新设置', 'administrator','refresh_alicdn', 'refresh_alicdn_settings_page');
 
 }
